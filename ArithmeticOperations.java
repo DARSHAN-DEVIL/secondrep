42abd91a940906848a41ddd7acdcddf7aa309462
DARSHAN-DEVIL <darshandeepakjanganure1234@gmail.com>
2025-04-29 16:44:44 +0530

    new

---
+++ ArithmeticOperations.java
@@ -0,0 +1,23 @@
+package java;
+
+class ArithmeticOperations {
+    public static void main(String[] args) {
+        // Hardcoded values
+        int a = 20;
+        int b = 5;
+
+        // Performing arithmetic operations
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double quotient = (double) a / b;
+
+        // Displaying results
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
